033fdcb5948111a945aaec30a0a99d3476df5b01
TobiasClausen <tobias.clausen06@gmail.com>
2025-04-15 14:26:03 +0200

    e3

--- cypress/e2e/d2.cy.js
+++ cypress/e2e/d2.cy.js
@@ -1,7 +1,7 @@
 describe('D2', () => {
   it('Der Login-Button ist disabled, wenn keine Infos eingegeben wurden', () => {
     cy.visit('/login')
-
+    
     cy.get('.login .btn').should('have.attr', 'disabled')
 
     cy.get('#email').type('user@example.com')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,42 @@
 <script setup>
+import { ref, onMounted } from 'vue'
 import LoginInfo from '@/components/LoginInfo.vue'
 import Composer from '@/components/Composer.vue'
 import Tweet from '@/components/Tweet.vue'
+import { fetchStream } from '../api/requests'
+
+const loading = ref(true)
+const tweets = ref([])
+
+onMounted(async () => {
+    loading.value = true
+    try {
+        const stream = await fetchStream()
+        tweets.value = stream
+    } catch (error) {
+        console.error(error)
+    } finally {
+        loading.value = false
+    }
+})
 </script>
 
 <template>
   <div>
     <LoginInfo />
     <Composer />
-    <Tweet 
-      v-for="tweet in [1, 2, 3, 4, 5, 6, 7, 8, 9]"
-      :user="{ id: 1, name: 'John Doe'}"
-      :text="'Lorem ipsum dolor med'"
-      :created-at="'2023-01-01T12:00:00Z'"
-    />
+    <div v-if="loading" class="loading">
+      Lade Tweets...
+    </div>
+    <div v-else class="stream">
+      <Tweet 
+        v-for="tweet in tweets"
+        :key="tweet.id"
+        :user="tweet.user"
+        :text="tweet.text"
+        :created-at="tweet.createdAt"
+      />
+    </div>
   </div>
 </template>
 
