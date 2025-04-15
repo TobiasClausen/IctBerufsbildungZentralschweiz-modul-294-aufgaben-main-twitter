ced86d0358eeefd6c5121470d26fd5e903f052c1
TobiasClausen <tobias.clausen06@gmail.com>
2025-04-15 10:32:36 +0200

    Initial commit

---
+++ src/components/Logo.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="logo">
+    Tw<strong>IT</strong>ter
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,17 @@
+<script setup>
+import IconLogin from './icons/IconLogin.vue'
+import IconHome from './icons/IconHome.vue'
+</script>
+
+<template>
+  <nav class="nav">
+    <a href="#" class="router-link-active">
+      <IconHome />
+      Stream
+    </a>
+    <a href="#">
+      <IconLogin />
+      Login
+    </a>
+  </nav>
+</template>
